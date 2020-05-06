c712092de1f9f1c494177b14428cd204d955c3e0
TheRealKS <koen.sauren@gmail.com>
2020-05-06 17:20:36 +0200

    client?

--- client_app.py
+++ client_app.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import argparse
+import time
 from btcp.client_socket import BTCPClientSocket
 
 
@@ -13,10 +14,30 @@ def main():
 
     # Create a bTCP client socket with the given window size and timeout value
     s = BTCPClientSocket(args.window, args.timeout)
-    # TODO Write your file transfer clientcode using your implementation of BTCPClientSocket's connect, send, and disconnect methods.
+    s.connect()
+
+    try:
+        f = open(args.input)
+        lines = f.read().encode()
+        sendFile(lines, s, 0)
+    except IOError:
+        print("File does not exist!")
+    finally:
+        f.close()
 
     # Clean up any state
-    s.close()
+    s.disconnect()
+
+
+def sendFile(filedata, socket, sent):
+    try:
+        socket.send(filedata)
+    except ValueError as e:
+        size = e.args[0]
+        slice = filedata[sent, size]
+        sendFile(slice, socket, 0)
+        if len(filedata) > 0:
+            sendFile(filedata, socket, size)
 
 
 main()
